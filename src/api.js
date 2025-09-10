6ab12d3b96ced48b5d8854e11927d0ea4a77799d
Femi2720 <femi65669@gmail.com>
2025-09-10 19:34:39 +0530

    set url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4000',
+  baseURL: process.env.REACT_APP_API_URL || 'https://assignment1-backendcode.onrender.com/',
   headers: { 'Content-Type': 'application/json' }
 });
 
