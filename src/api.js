e0cc1f2bdd23955f600672d15212c1efe8d73012
Femi2720 <femi65669@gmail.com>
2025-09-10 20:39:15 +0530

    fixed error

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://assignment1-backendcode.onrender.com/',
+  baseURL: process.env.REACT_APP_API_URL || 'https://assignment1-backendcode.onrender.com',
   headers: { 'Content-Type': 'application/json' }
 });
 
